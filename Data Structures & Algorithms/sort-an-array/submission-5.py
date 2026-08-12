9ce5015010fa0b32c7a04c375a253e7406c4050b
Samarjit Debnath <45074297+SamarjitDebnath@users.noreply.github.com>
2026-08-12 15:46:01 +0530

    Add: sort-an-array - submission-5

---
+++ Data Structures & Algorithms/sort-an-array/submission-5.py
@@ -0,0 +1,40 @@
+from random import randint
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def _swap(arr, i, j):
+            arr[i], arr[j] = arr[j], arr[i]
+
+        def _partition(arr, low, high):
+            random_index = randint(low, high)
+
+            _swap(arr, random_index, high)
+            pivot = arr[high]
+
+            # smallest tracker
+            i = low - 1
+
+            for j in range(low, high):
+                if arr[j] < pivot:
+                    i += 1
+                    _swap(arr, i, j)
+
+            # placing pivot to correct position
+            pi = i + 1
+            _swap(arr, pi, high)
+
+            return pi
+
+        def _quickSort(arr, low, high):
+            if low < high:
+                pi = _partition(arr, low, high)
+
+                # sort left
+                _quickSort(arr, low, pi-1)
+
+                # sort right
+                _quickSort(arr, pi+1, high)
+
+        _quickSort(nums, 0, len(nums)-1)
+
+        return nums
+            
